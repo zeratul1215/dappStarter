d8af608b515e1c7ea98136c2185e45e14137c891
zeratul1215 <ztianhong7@gmail.com>
2025-07-10 11:46:00 +0900

    frontend starter

---
+++ dapp-front/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Hello dapp</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ dapp-front/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@hooks': '/src/hooks',
+      '@utils': '/src/utils',
+      '@types': '/src/types',
+      '@constants': '/src/constants',
+      '@assets': '/src/assets',
+    },
+  },
+})
