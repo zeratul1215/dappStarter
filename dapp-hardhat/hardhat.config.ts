255bd4348446755763f0f780eec6b0d6d3683e1b
zeratul1215 <ztianhong7@gmail.com>
2025-07-11 13:53:57 +0900

    harhat config

--- dapp-hardhat/hardhat.config.ts
+++ dapp-hardhat/hardhat.config.ts
@@ -1,8 +1,22 @@
 import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    }
+  }
 };
 
 export default config;
