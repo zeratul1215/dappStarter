4840379c3cf3839a6f09147b52b590b40371fba5
zeratul1215 <ztianhong7@gmail.com>
2025-07-11 22:03:12 +0900

    dep update

--- dapp-hardhat/hardhat.config.ts
+++ dapp-hardhat/hardhat.config.ts
@@ -1,6 +1,10 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-ignition"
+import "solidity-coverage"
+import "hardhat-gas-reporter"
+import "hardhat-deploy"
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -17,7 +21,14 @@ const config: HardhatUserConfig = {
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
     }
-  }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
 };
 
 export default config;
