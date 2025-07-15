ea911eb9529b5a123b3a11eccc5837f660561529
zeratul1215 <ztianhong7@gmail.com>
2025-07-15 16:12:57 +0900

    config update

--- dapp-hardhat/hardhat.config.ts
+++ dapp-hardhat/hardhat.config.ts
@@ -1,14 +1,31 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
-import "@nomicfoundation/hardhat-ignition"
-import "solidity-coverage"
-import "hardhat-gas-reporter"
-import "hardhat-deploy"
-
+import "@nomicfoundation/hardhat-ignition";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-deploy";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-  defaultNetwork: "localhost",
+  // solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+      },
+      {
+        version: "0.6.6",
+      }
+    ]
+  },
+  defaultNetwork: "hardhat",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      sepolia: 0,
+    }
+  },
   networks: {
     hardhat: {
     },
@@ -20,14 +37,15 @@ const config: HardhatUserConfig = {
       url: process.env.SEPOLIA_URL || "",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
+      // blockConfirmations: 6,
     }
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    enabled: true,
+    currency: "USD"
   },
 };
 
